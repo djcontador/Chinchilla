3bafd86a32c2ac7367ab93a2bbfeb215448f06a7
Daniela Contador Zanforlin <djcontador@uc.cl>
2017-03-19 21:47:06 -0300

    19 de marzo: modulo 3

---
+++ Tarea 0/mod3.py
@@ -0,0 +1,5 @@
+# definir record tiempo
+# definir funcion calcular distancias entre tiempos
+# definir tipo usuario (ANAF o que recurso)
+
+#usuario ANAF: agregar nuevos usuarios, incendios, meteorologias,
